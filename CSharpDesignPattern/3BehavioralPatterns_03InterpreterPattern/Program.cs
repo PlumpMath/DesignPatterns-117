f28237c327e3fe267a9ed738b8389072db36f9ab
tudm <dmt.20102514@gmail.com>
2016-04-22 03:58:13 +0700

    import source

---
+++ CSharpDesignPattern/3BehavioralPatterns_03InterpreterPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BehavioralPatterns_03InterpreterPattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ CSharpDesignPattern/3BehavioralPatterns_08StrategyPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BehavioralPatterns_08StrategyPattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
